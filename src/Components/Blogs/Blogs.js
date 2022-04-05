f4f18462682ac7624a5c9715dd9f916a25c6f54a
wakeupzaman909 <hadiuzzaman909@outlook.com>
2022-04-05 22:54:41 +0600

    Blogs Updated

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -10,7 +10,7 @@ const Blogs = () => {
                   <div class="accordion-item">
                     <h2 class="accordion-header" id="headingOne">
                       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                      What is Context API? How does it works?
+                      What is Context API?
                       </button>
                     </h2>
                     <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
@@ -22,7 +22,7 @@ const Blogs = () => {
                   <div class="accordion-item">
                     <h2 class="accordion-header" id="headingTwo">
                       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        What is Semantic Tag?Why do it need to use?
+                        What is Semantic Tag?
                       </button>
                     </h2>
                     <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
